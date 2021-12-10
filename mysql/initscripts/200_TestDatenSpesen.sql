d184bf7cafa3b25b7fdd2e3fd00e8e0d2cd62a23
remooooooo <remostark@bluewin.ch>
2021-12-10 10:09:51 +0000

    Mehr Testdaten hinzugefügt | Kleine anpassung am View

--- mysql/initscripts/200_TestDatenSpesen.sql
+++ mysql/initscripts/200_TestDatenSpesen.sql
@@ -1,4 +1,7 @@
 INSERT INTO `SpesenFormular` (`ID`, `Personalnummer`,`UserID`, `Datum`, `Reiseziel`, `Essenskosten`, `Fahrtkosten`, `KM_Anzahl`, `Uebernachtung`, `Verkehrsmittel`, `Unterschrift`) VALUES
 (1,	'100000',2,	'2021-11-29 13:42:47',	'Jowa Gossau',	20,	25,	0,	30,	'Zug',	0),
-(2,	'100001',2,	'2021-11-29 13:44:18',	'Graenichen Jowa',	10,	15,	0,	40,	'Zug',	1),
-(3,	'100002',1,	'2021-11-29 13:47:16',	'Bischofszell BINA',	0,	0,	10,	0,	'Auto',	1);
+(2,	'100001',2,	'2021-10-29 13:44:18',	'Industriestrasse 6 Bischofszell',	10,	15,	0,	40,	'Zug',	1),
+(3,	'100002',2,	'2021-11-29 13:44:18',	'Graenichen Jowa',	10,	15,	0,	40,	'Zug',	1),
+(4,	'100001',2,	'2021-11-29 13:44:18',	'JOWA Zuerich',	0,	0,	10,	0,	'Auto',	1),
+(5,	'000003',1,	'2021-12-29 13:47:16',	'Aproz',	0,	0,	10,	0,	'Auto',	1),
+(6,	'000025',1,	'2021-12-29 13:47:16',	'Bischofszell BINA', 20,	25,	0,	30,	'Zug',	0);
